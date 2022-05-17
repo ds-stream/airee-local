4d05fbc426bb094e59631f16595d3ab716bf4fb6
Bakiera <kar.bakiera@gmail.com>
2022-05-17 12:41:45 +0200

    delete.sh help comamd

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,10 +1,29 @@
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 set -euo pipefail
 
+Help()
+{
+   # Display Help
+   echo "To delete a dag use command as follow:."
+   echo
+   echo "./delete.sh <dag_name_in_local_dag_folder> <dag_id>"
+   echo 
+   echo
+}
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
 export COMPOSE_FILE="${PROJECT_DIR}/docker-compose.yaml"
 if [ $# -eq 2 ]; then
-    sudo rm /home/karolbakiera/github/airflow-on-k8s/airflow/dags/$2.py
+    sudo rm ../dags/$2.py
     exec docker-compose run --rm airflow-cli airflow dags delete $1
 else
-    echo "pass dag id"
+    echo "Insufficient number of arguments. See:"
+    echo
+    echo "./delete.sh -h"
+    echo
 fi
